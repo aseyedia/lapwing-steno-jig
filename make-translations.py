7c6ad590f20a4d212c628d0614ad07473b09ff26
Lynn <lynn@foldr.moe>
2024-02-13 15:07:50 +0100

    Replace hints with Lapwing

---
+++ make-translations.py
@@ -0,0 +1,17 @@
+import json
+from collections import defaultdict
+
+with open("steno-dictionaries/lapwing-base.json") as f1:
+    with open("steno-dictionaries/lapwing-proper-nouns.json") as f2:
+        lapwing = {**json.load(f1), **json.load(f2)}
+
+inverse = defaultdict(list)
+for k, v in lapwing.items():
+    inverse[v].append(k)
+
+def key(k):
+    return (-k.count('/'), -len(k))
+
+inverse = {v: sorted(ks, key=key) for v, ks in inverse.items()}
+
+print(f'''TypeJig.Translations.Plover = {json.dumps(inverse)};''')
