0c07707633ef7b6e283a0f07c5e996df2fb50561
Arta Seyedian <arta.seyedian@gmail.com>
2024-07-23 07:58:55 -0400

    removing the longest write out to prevent conflicts and such

--- make-translations.py
+++ make-translations.py
@@ -12,7 +12,11 @@ for k, v in lapwing.items():
 def key(k):
     return (-k.count('/'), -len(k))
 
-inverse = {v: sorted(ks, key=key) for v, ks in inverse.items()}
+# Modify the inverse dictionary to remove the first element from each list of values if the list has more than one element
+for v, ks in inverse.items():
+    if len(ks) > 1:
+        sorted_ks = sorted(ks, key=key)
+        inverse[v] = sorted_ks[1:]
 
 with open("lapwing-translations.js", "w") as f:
     f.write(f'TypeJig.Translations.Lapwing = {json.dumps(inverse)};')
